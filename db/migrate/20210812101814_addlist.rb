d891abcfec4de31f013669892a26a46f3b3dd650
Thibault Fitte-Duval <thibault.fitteduval.tfd@gmail.com>
2021-08-12 14:49:23 +0200

    model clean

--- db/migrate/20210812101814_addlist.rb
+++
@@ -1,7 +0,0 @@
-class Addlist < ActiveRecord::Migration[6.0]
-  def change
-    add_column :bookmarks, :comment, :text
-    t.references :movie, null: false, foreign_key: true
-    t.references :list, null: false, foreign_key: true
-  end
-end
