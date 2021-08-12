9baf4b8635470ab4afea801dbc1f5230e76845d7
Thibault Fitte-Duval <thibault.fitteduval.tfd@gmail.com>
2021-08-12 14:05:21 +0200

    add bookmarks

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,5 @@
 class Bookmark < ApplicationRecord
-   belongs_to :movie
+  belongs_to :movie
   belongs_to :list
   validates :movie, uniqueness: { scope: :list }
   validates :comment, length: { minimum: 6 }
